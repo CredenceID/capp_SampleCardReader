60214e89dd6fbb8c08b589d8469ab942f09e7af7
Avi Varshney <avarshney65@gmail.com>
2018-11-21 13:38:50 -0800

    Updated to latest service/jar.

--- app/src/main/java/com/credenceid/sample/cardreader/Hex.java
+++ app/src/main/java/com/credenceid/sample/cardreader/Hex.java
@@ -1,4 +1,4 @@
-package com.cid.sample.cardreader;
+package com.credenceid.sample.cardreader;
 
 /* This is a helper class used for converting bytes into human readable Hex strings. */
 @SuppressWarnings("WeakerAccess")
