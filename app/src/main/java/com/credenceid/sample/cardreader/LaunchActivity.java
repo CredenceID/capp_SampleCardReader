b905eaf8335ecbe1194b2429dd529d93b078d817
Avi Varshney <avarshney65@gmail.com>
2019-04-25 16:07:44 -0700

    Updated.

--- app/src/main/java/com/credenceid/sample/cardreader/LaunchActivity.java
+++ app/src/main/java/com/credenceid/sample/cardreader/LaunchActivity.java
@@ -1,59 +1,22 @@
 package com.credenceid.sample.cardreader;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.credenceid.biometrics.Biometrics.ResultCode;
+import com.credenceid.biometrics.Biometrics;
 import com.credenceid.biometrics.BiometricsManager;
-import com.credenceid.biometrics.DeviceFamily;
-import com.credenceid.biometrics.DeviceType;
 
 import static android.widget.Toast.LENGTH_LONG;
 import static android.widget.Toast.LENGTH_SHORT;
 import static com.credenceid.biometrics.Biometrics.ResultCode.FAIL;
+import static com.credenceid.biometrics.Biometrics.ResultCode.INTERMEDIATE;
 import static com.credenceid.biometrics.Biometrics.ResultCode.OK;
 
-@SuppressLint("StaticFieldLeak")
-@SuppressWarnings({"unused"})
+@SuppressWarnings({"unused", "StaticFieldLeak", "StatementWithEmptyBody"})
 public class LaunchActivity
 		extends Activity {
-
-	private static final String TAG = LaunchActivity.class.getSimpleName();
-
-	/* CredenceSDK biometrics object, used to interface with APIs. */
-	private static BiometricsManager mBiometricsManager;
-	/* Stores which Credence family of device's this app is running on. */
-	private static DeviceFamily mDeviceFamily = DeviceFamily.InvalidDevice;
-	/* Stores which specific device this app is running on. */
-	private static DeviceType mDeviceType = DeviceType.InvalidDevice;
-
-	/* --------------------------------------------------------------------------------------------
-	 *
-	 * Public getter methods.
-	 *
-	 * --------------------------------------------------------------------------------------------
-	 */
-
-	public static BiometricsManager
-	getBiometricsManager() {
-
-		return mBiometricsManager;
-	}
-
-	public static DeviceType
-	getDeviceType() {
-
-		return mDeviceType;
-	}
-
-	public static DeviceFamily
-	getDeviceFamily() {
-
-		return mDeviceFamily;
-	}
 
 	/* --------------------------------------------------------------------------------------------
 	 *
@@ -81,28 +44,31 @@ public class LaunchActivity
 	initBiometrics() {
 
 		/*  Create new biometrics object. */
-		mBiometricsManager = new BiometricsManager(this);
+		App.BioManager = new BiometricsManager(this);
 
 		/* Initialize object, meaning tell CredenceService to bind to this application. */
-		mBiometricsManager.initializeBiometrics((ResultCode resultCode,
-												 String minimumVersion,
-												 String currentVersion) -> {
-
-			/* This API will never return ResultCode.INTERMEDIATE. */
+		App.BioManager.initializeBiometrics((Biometrics.ResultCode resultCode,
+											 String minimumVersion,
+											 String currentVersion) -> {
 
 			if (OK == resultCode) {
 				Toast.makeText(this, getString(R.string.biometrics_initialized), LENGTH_SHORT).show();
 
-				mDeviceFamily = mBiometricsManager.getDeviceFamily();
-				mDeviceType = mBiometricsManager.getDeviceType();
+				App.DevFamily = App.BioManager.getDeviceFamily();
+				App.DevType = App.BioManager.getDeviceType();
 
 				/* Launch main activity. */
 				Intent intent = new Intent(this, CardReaderActivity.class);
 				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 				startActivity(intent);
 				this.finish();
-			} else if (FAIL == resultCode)
+
+			} else if (INTERMEDIATE == resultCode) {
+				/* This code is never returned here. */
+
+			} else if (FAIL == resultCode) {
 				Toast.makeText(this, getString(R.string.biometrics_fail_init), LENGTH_LONG).show();
+			}
 		});
 	}
 }
